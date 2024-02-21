d1ba9a966b8835865a567299878c69d8849c41f6
TOMCHRISTER\tomcs <tomchrister.s@outlook.com>
2024-02-21 17:11:32 +0100

    added a confirmation page

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,29 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Retrieve cart data from localStorage
-    const cartData = localStorage.getItem('cart');
+    const cartData = localStorage.getItem('cartDataForConfirmation');
     // Parse the JSON string to get the cart array
     const cartArray = JSON.parse(cartData);
 
     // Display cart information (for example, in a list)
-    const cartListElement = document.getElementById('cartList');
+    const cartListElement = document.getElementById('confirmationCartList');
     if (cartArray && cartArray.length > 0) {
         cartArray.forEach(item => {
             const listItem = document.createElement('li');
             listItem.textContent = `${item.title} - $${item.price} (Quantity: ${item.quantity})`;
             cartListElement.appendChild(listItem);
         });
-
-        // Add purchase button
-        const purchaseButton = document.createElement('button');
-        purchaseButton.textContent = 'Purchase';
-        purchaseButton.addEventListener('click', function() {
-            // Store cart data in localStorage
-            localStorage.setItem('cartDataForConfirmation', JSON.stringify(cartArray));
-            // Redirect to order confirmation page
-            window.location.href = 'confirmation.html'; // Change the URL to your order confirmation page
-        });
-        cartListElement.appendChild(purchaseButton);
-
     } else {
         // Handle case when cart is empty
         const emptyCartMessage = document.createElement('p');
@@ -31,5 +19,3 @@ document.addEventListener('DOMContentLoaded', function() {
         cartListElement.appendChild(emptyCartMessage);
     }
 });
-
-
